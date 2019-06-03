c62415d519ff32b54e11509a6c3b3528774cff50
freeWillee <will.ik.lee@gmail.com>
2019-06-03 15:06:34 -0600

    Remove relationships for task_serializer

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -2,7 +2,4 @@ class TaskSerializer
   include FastJsonapi::ObjectSerializer
   
   attributes :title, :content, :percent_complete, :due_date, :user_id, :project_id
-
-  belongs_to :user
-  belongs_to :project
 end
